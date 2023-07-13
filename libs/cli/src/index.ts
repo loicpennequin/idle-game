a3cbaf9fc2b51498d728258e8bea7532c0bc3d8e
loicpennequin <lo.pennequin@gmail.com>
2023-07-13 16:16:07 +0200

    added setup CLI and readme

---
+++ libs/cli/src/index.ts
@@ -0,0 +1,36 @@
+#!/usr/bin/env node
+
+import { setup } from './commands/setup';
+import { logger } from './gui.js';
+import { Command } from 'commander';
+
+(async function () {
+  const program = new Command().name('Daria-CLI');
+  program
+    .command('feature')
+    .description('Creates a new feature')
+    .argument(
+      '<name>',
+      'The name of the feature you wish to create. It will be the name of the folder the created files will go to.'
+    )
+    .action(name => {
+      console.log('feature command not implemented yet');
+    });
+
+  program.command('setup').description('Setup the project').action(setup);
+
+  program.parseAsync();
+
+  process.exit(0);
+})().catch(err => {
+  logger.error('Aborting...');
+  if (err instanceof Error) {
+    logger.error(err);
+  } else {
+    logger.error(
+      'An unknown error has occurred. Please open an issue on github with the informations below:'
+    );
+    console.log(err);
+  }
+  process.exit(1);
+});
